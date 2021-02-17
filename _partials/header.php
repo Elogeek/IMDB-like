b7b43b0b698631bbb07f601debb0883f1a8eb7e2
Elogeek <elodiechristin@gmail.com>
2021-02-17 16:05:35 +0100

    carousel synopsis

--- _partials/header.php
+++ _partials/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>IMBD-Like: ma version</title>
     <script src="https://kit.fontawesome.com/e3ddf954eb.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../asset/css/style.css">
     <link rel="stylesheet" href="../asset/css/page2.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
